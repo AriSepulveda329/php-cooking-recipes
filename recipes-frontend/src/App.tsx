07c0e4e551a962594aefdb5e6839bf08c3339d55
ari alonso sepulveda rosales <alonso.ari.329@gmail.com>
2024-08-29 16:12:21 -0600

    Add global ts file

--- recipes-frontend/src/App.tsx
+++ recipes-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="bg-gray-200 h-screen p-5">
-      <header className="flex justify-between">
+      <header className="flex justify-between mb-5">
         <h1 className="text-2xl font-bold">My cooking recipes (100)</h1>
         <div className="flex bg-white w-1/2 py-1 px-3 rounded-full">
           <button className="p">
@@ -16,6 +18,15 @@ function App() {
           ></input>
         </div>
       </header>
+      <nav className="flex font-medium justify-between">
+        <div className="flex gap-10">
+          <button className={`${styles.underlineHover}`}>All</button>
+          <button className={`${styles.underlineHover}`}>Favorites</button>
+        </div>
+        <button>
+          Filter <FilterAltIcon sx={{ color: "black" }} />
+        </button>
+      </nav>
     </div>
   );
 }
---
+++ recipes-frontend/src/Global.d.ts
@@ -0,0 +1,4 @@
+declare module "*.module.css" {
+  const Styles: { [className: string]: string };
+  export default Styles;
+}
